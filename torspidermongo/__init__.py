b88f73c5c9bc2e226e8a53167845281c195ca67c
Sergey Krushinsky <krushinsky@gmail.com>
2017-10-29 04:46:07 +0300

    debug line

--- torspidermongo/__init__.py
+++ torspidermongo/__init__.py
@@ -1,11 +1,11 @@
 from .client import MongoClient
-
+import logging
 
 client = None
 
 def init_client(io_loop=None, db='mongodb://localhost:27017/torspider'):
     global client
-
+    logging.debug('Initializing client. db: %s', db)
     MongoClient.setup(db=db, io_loop=io_loop)
     client = MongoClient()
 
